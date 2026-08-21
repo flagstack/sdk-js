647c65310aeb637636c5def957907edc583507ba
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-21 23:30:38 +0100

    node: Discard realtime refresh result

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -41,7 +41,9 @@ export class NodeSwitchOnYourCodeClient extends SwitchOnYourCodeClient {
       baseUrl: clientOptions.baseUrl,
       sdkKey: normalizedKey,
       fetch: clientOptions.fetch ?? globalThis.fetch,
-      onConfigurationChanged: () => this.refresh(),
+      onConfigurationChanged: async () => {
+        await this.refresh()
+      },
       onError: clientOptions.onError,
     }
     if (realtimeReconnectDelayMs !== undefined) {
